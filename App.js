c4b1fac95431920a075a35ecabbc51408cdef226
Cael7 <carlosdomingues.ads@gmail.com>
2023-07-09 15:00:18 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import React from "react";
+import CurrentPrice from "./src/components/CurrentPrice";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style backgroundColor="#f50d41" barStyle="dark-content" />
+      <CurrentPrice />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? 40 : 0,
   },
 });
